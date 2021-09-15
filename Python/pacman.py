d884472f054d3723911b1b1627396e3e70e8b7f5
Miguel Arriaga <marriagav3@gmail.com>
2021-09-15 11:32:31 -0500

    added two more ghosts

--- Python/pacman.py
+++ Python/pacman.py
@@ -24,6 +24,8 @@ ghosts = [
     [vector(-180, -160), vector(0, 5)],
     [vector(100, 160), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
+    [vector(50, 160), vector(-5, 0)],
+    [vector(50, -160), vector(-5, 0)]
 ]
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
