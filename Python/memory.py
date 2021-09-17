e7e8ba31b6b54445ac32f3615aa4e2993e613950
Francisco_Herrera <pacoherrera218@gmail.com>
2021-09-17 09:22:34 -0500

    invert-board-color

--- Python/memory.py
+++ Python/memory.py
@@ -17,7 +17,7 @@ from freegames import path
 car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
-hide = [True] * 64
+hide = [True] * 16
 tapCount=0
 
 def square(x, y):
@@ -25,7 +25,7 @@ def square(x, y):
     up()
     goto(x, y)
     down()
-    color('black', 'white')
+    color('white', 'black')
     begin_fill()
     for count in range(4):
         forward(50)
@@ -34,11 +34,11 @@ def square(x, y):
 
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
-    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+    return int((x + 200) // 50 + ((y + 200) // 50) * 4)
 
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
-    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+    return (count % 4) * 50  - 200, (count // 4) * 50 - 200
 
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
@@ -62,7 +62,7 @@ def draw():
     shape(car)
     stamp()
 
-    for count in range(64):
+    for count in range(16):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
@@ -73,7 +73,7 @@ def draw():
         x, y = xy(mark)
         up()
         goto(x + 2, y)
-        color('black')
+        color('white')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
     update()
